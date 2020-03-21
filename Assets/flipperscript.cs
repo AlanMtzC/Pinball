dca53b47878f761a50d9fffcd2e05b06689f619a
Alan Mtz <alan9mtz@gmail.com>
2020-03-20 17:44:24 -0700

    first commit

---
+++ Assets/flipperscript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flipperscript : MonoBehaviour
+{
+    public float restPosition = 0f;
+    public float pressedPosition = 45f;
+    public float hitStrength = 10000f;
+    public float flipperDamper = 150f;
+
+    public string inputName;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if(input.GetAxis(inputName) == 1)
+        {
+
+        }
+    }
+}
