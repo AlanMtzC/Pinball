d26735e7eacb8d0bcd81ca725200cc0f127c11e4
Alan Mtz <alan9mtz@gmail.com>
2020-04-22 10:10:24 -0600

    commit de axis

--- Assets/flipperscript.cs
+++ Assets/flipperscript.cs
@@ -6,8 +6,10 @@ public class flipperscript : MonoBehaviour
 {
     public float restPosition = 0f;
     public float pressedPosition = 45f;
-    public float hitStrength = 10000f;
+    public float hitStrenght = 10000f;
     public float flipperDamper = 150f;
+    public int axisValue = 1;
+
     HingeJoint hinge;
     public string inputName;
 
@@ -15,14 +17,27 @@ public class flipperscript : MonoBehaviour
     void Start()
     {
         hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
     }
 
 
     void Update()
     {
-        if(Input.GetAxis(inputName) == 1)
-        {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrenght;
+        spring.damper = flipperDamper;
 
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+        if(Input.GetAxisRaw("Horizontal")== axisValue)
+        //if(Input.GetButton(inputName))
+        {
+            spring.targetPosition = pressedPosition;
         }
+        else
+        {
+            spring.targetPosition = restPosition;
+        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
     }
 }
